f37d85496445acad1bc18fe1258f19958ec86be2
Mark Garnett <thecommunistduck@hotmail.co.uk>
2014-06-11 23:24:16 +0100

    Added unit test fixtures and action shells.
    Added blank files, licenses, test attributes, etc.

---
+++ ICSharpCode.NRefactory.CSharp.Refactoring/CodeActions/AddNewModifierToMethodAction.cs
@@ -0,0 +1,55 @@
+﻿//
+// AddNewModifierToMethodAction.cs
+//
+// Author:
+//       Mark Garnett <mg10g13@soton.ac.uk>
+//
+// Copyright (c) 2014 Mark Garnett (mg10g13@soton.ac.uk)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using ICSharpCode.NRefactory6.CSharp.Refactoring;
+using Microsoft.CodeAnalysis.CSharp;
+using ICSharpCode.NRefactory6.CSharp;
+
+namespace ICSharpCode.NRefactory.CSharp.Refactoring
+{
+    /// <summary>
+    /// Adds the new modifier to a method in a derived class.
+    /// </summary>
+    [NRefactoryCodeRefactoringProvider(Description = "Add the new modifier to a method.")]
+    [ExportCodeRefactoringProvider("Add the new modifier to a method.", LanguageNames.CSharp)]
+    public class AddNewModifierToMethodAction : ICodeRefactoringProvider
+    {
+        public Task<IEnumerable<CodeAction>> GetRefactoringsAsync(Document document, TextSpan span, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ICSharpCode.NRefactory.CSharp.Refactoring/CodeActions/ConvertIndexerToMethodAction.cs
@@ -0,0 +1,54 @@
+﻿//
+// ConvertIndexerToMethodAction.cs
+//
+// Author:
+//       Mark Garnett <mg10g13@soton.ac.uk>
+//
+// Copyright (c) 2014 Mark Garnett (mg10g13@soton.ac.uk)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using ICSharpCode.NRefactory6.CSharp.Refactoring;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ICSharpCode.NRefactory.CSharp.Refactoring
+{
+    /// <summary>
+    /// Converts an indexer method to a method.
+    /// </summary>
+    [NRefactoryCodeRefactoringProvider(Description = "Converts an indexer method to a method.")]
+    [ExportCodeRefactoringProvider("Converts an indexer method to a method.", LanguageNames.CSharp)]
+    public class ConvertIndexerToMethodAction : ICodeRefactoringProvider
+    {
+        public Task<IEnumerable<CodeAction>> GetRefactoringsAsync(Document document, TextSpan span, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
